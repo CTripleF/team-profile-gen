f8b74c4882fc4e155cbc07a880c218fc1b335ce0
CTripleF <cjfabra1@gmail.com>
2021-12-31 16:34:33 -0500

    wtrite to html test and fixes

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,52 +1,55 @@
 
 const generateManager= (employee) => {
   return `
-  <div class = 'col-4 mt-4'>
-    <div class = 'card h-100'>
-      <div class = 'card-header'>
-        <h2>${employee.name}</h2>
-        <h3>Manager</h3>
-      </div>
+<div class = 'col-4 mt-4'>
+  <div class = 'card h-100'>
+    <div class = 'card-header'>
+      <h2>${employee.name}</h2>
+      <h3>Manager</h3>
+    </div>
     <div class = 'card-body'>
       <p id = 'id'>ID: ${employee.id}</p>
       <p id = 'email'>E-mail: <a href="mailto: ${employee.email}">${employee.email}</a></p>
       <p id = 'officeId'>Office ID: ${employee.officeId}</p>
     </div>
   </div>
+</div>
   `;
 }
 
 const generateEngineer= (employee) => {
   return `
-  <div class = 'col-4 mt-4'>
-    <div class = 'card h-100'>
-      <div class = 'card-header'>
-        <h2>${employee.name}</h2>
-        <h3>Engineer</h3>
-      </div>
+<div class = 'col-4 mt-4'>
+  <div class = 'card h-100'>
+    <div class = 'card-header'>
+      <h2>${employee.name}</h2>
+      <h3>Engineer</h3>
+    </div>
     <div class = 'card-body'>
       <p id = 'id'>ID: ${employee.id}</p>
       <p id = 'email'>E-mail: <a href="mailto: ${employee.email}">${employee.email}</a></p>
       <p id = 'github'>Github: <a href="https://github/${employee.github}">${employee.github}</a></p>
     </div>
   </div>
+</div>
   `;
 }
 
 const generateIntern= (employee) => {
   return `
-  <div class = 'col-4 mt-4'>
-    <div class = 'card h-100'>
-      <div class = 'card-header'>
-        <h2>${employee.name}</h2>
-        <h3>Intern</h3>
-      </div>
+<div class = 'col-4 mt-4'>
+  <div class = 'card h-100'>
+    <div class = 'card-header'>
+      <h2>${employee.name}</h2>
+      <h3>Intern</h3>
+    </div>
     <div class = 'card-body'>
       <p id = 'id'>ID: ${employee.id}</p>
       <p id = 'email'>E-mail: <a href="mailto: ${employee.email}">${employee.email}</a></p>
       <p id = 'school'>School: ${employee.officeId}</p>
     </div>
   </div>
+</div>
   `;
 }
 
@@ -84,27 +87,29 @@ return generateTeam;
 // base template for HTML page using collected user info for card outputs
 const generateTeamPage = function (employeeCards)  {
   return `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Team Profile</title>
-      <link rel="stylesheet" href="styles.css">
-  </head>
-  <body>
-      <header>
-          <nav class="navbar" id="navbar">
-              <span class="navbar-brand mb-0 h2 2-100 text-center" id="navbar-text">Team Profile</span>
-          </nav>
-      </header>
-      <main>
-          <div class="container">
-              <div class="row justify-content-center" id="team-cards">
-              ${employeeCards}
-          </div>
-      </main>    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Team Profile</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+  <header>
+    <nav class="navbar" id="navbar">
+      <span class="navbar-brand mb-0 h2 2-100 text-center" id="navbar-text">Team Profile</span>
+    </nav>
+  </header>
+  <main>
+    <div class="container">
+      <div class="row justify-content-center" id="team-cards">
+      ${employeeCards}
+    </div>
+  </div>
+  </main>    
 </body>
 </html>
   
